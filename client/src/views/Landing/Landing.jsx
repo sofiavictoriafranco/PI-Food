c1827cfd7c7379e92243a9c56cfc77e7e672683a
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2023-04-18 19:20:39 -0300

    resuelto

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,11 +1,20 @@
+import styles from './Landing.module.css'
+
+
+
 const Landing = () => {
+
     return(
-        <>
+        < body className={styles.landingfondo}>
+
+     
 
         <h1>Landing</h1>
 
+       
 
-        </>
+
+        </body>
     )
 }
 
