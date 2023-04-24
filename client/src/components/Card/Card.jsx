60fa3470f8f1ca995da0c9ebc6c0989097cc15ed
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2023-04-24 16:07:00 -0300

    resuelto

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -5,10 +5,10 @@ const Card = (props) => {
     return(
         <div className={style.card}>
 
-            <Link to={`/detail/${props.id}`}>
+            <Link to={`/detail/${props.id}`} className={style.myButton}>
             
-            <p>Title:{props.title}</p>
-            <p>Diets:{props.diets}</p>
+            <p>Title: {props.title}</p>
+            <p>Diets: {props.diets}</p>
             <img src={props.image} alt={props.title}/>
             </Link>
 
