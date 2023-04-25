92cae64a6352b5e1b992768349de78e1c47595d1
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2023-04-24 22:02:54 -0300

    resuelto

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,13 +2,17 @@ import { Link } from 'react-router-dom'
 import style from './Card.module.css'
 
 const Card = (props) => {
+
+   const diets = props.diets.join(', ');
+
+
     return(
         <div className={style.card}>
 
             <Link to={`/detail/${props.id}`} className={style.myButton}>
             
             <p>Title: {props.title}</p>
-            <p>Diets: {props.diets}</p>
+            <p>Diets: {diets}</p>
             <img src={props.image} alt={props.title}/>
             </Link>
 
