ebe3441a20976d08eb72e7a3f50a8641ef43dcd2
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2023-04-25 13:19:20 -0300

    resuelto

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -26,8 +26,7 @@ const Detail = () => {
  
 
 
-    const detailSummary = detail && detail.summary ? detail.summary : "";
-    const summary = detailSummary.replace(/<[^>]*>/g, "");
+    
 
     
     
@@ -45,9 +44,9 @@ console.log(detail)
 
             <h1>Title: {detail && detail.title ? detail.title : ''}</h1>
             <h2>Id: {detail && detail.id ? detail.id : ''}</h2>
-            <h2>Summary:  {summary}</h2>
+            <h2>Summary:  {detail && detail.summary ? detail.summary.replace(/<[^>]*>/g, "") : ''}</h2>
             <h2>Health Score: {detail && detail.healthScore ? detail.healthScore : ''}</h2>
-            <h2>Instructions: {detail && detail.instructions ? detail.instructions : ''}</h2>
+            <h2>Instructions: {detail && detail.instructions ? detail.instructions.replace(/<[^>]*>/g, "") : ''} </h2>
             <h2>diets: {detail && detail.diets ? detail.diets.join(', ') : ""}</h2>
             <img src={detail.image} alt={detail.title}/>
 
