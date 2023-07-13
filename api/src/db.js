a03b6cacfcb139c524f2f4da559c52cb7c6b17bd
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2023-07-13 17:39:48 -0300

    resuelto

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_PORT,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/food`, {
+const sequelize = new Sequelize( `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/food`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
